c54191a27ac69dbb9aa4d40595be88151d4c4440
Adeniyi Fajemisin <adeniyifajemisin@gmail.com>
2026-03-25 18:37:00 +0100

    salary expectation

--- configuration/salary.py
+++ configuration/salary.py
@@ -1,4 +1,3 @@
-expected_salary = ''
+expected_salary = '750 usd/month' # Expected salary for the position, you can update it based on the requirements and market standards. also with the budget of the company and your experience level. This is just an example and should be adjusted to fit your specific situation.
 
-current_salary = ''
-
+current_salary = '550 usd/month' # Current salary, this should be converted to the actual currency equivalence.
